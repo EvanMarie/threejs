74bd1c61cb109d95dd05fe8df3b9acf781429d5f
Dustin Carr <dwcarr@gmail.com>
2024-03-15 23:13:55 -0400

    updated remix and dev tools

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { unstable_vitePlugin as remix } from "@remix-run/dev";
+import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { flatRoutes } from "remix-flat-routes";
-import { remixDevTools } from "remix-development-tools/vite";
+import { remixDevTools } from "remix-development-tools";
 
 export default defineConfig({
-    server: {
+  server: {
     port: 3000,
     host: true,
   },
